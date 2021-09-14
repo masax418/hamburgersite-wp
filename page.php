6aaeb91387d0bcdf333567070281254309c56c6b
masataka <masaburger0418@yahoo.co.jp>
2021-09-14 22:45:11 +0900

    firstcommit

---
+++ page.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- メディアクエリ -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- タブ題名 -->
+  <title>hamburger site / 固定ページ</title>
+  <!-- css読込み -->
+  <link rel="stylesheet" href="./css/style.css">
+  <!-- fontawesome -->
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <!-- Google Font -->
+  <!-- Roboto -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <!-- M PLUS 1p -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&display=swap" rel="stylesheet">
+  <!-- jQueryの読み込み -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+  <div class="l-grid">
+    <header class="l-header">
+      <div class="p-header">
+        <button class="c-btn--menu c-btn--js">Menu</button>
+        <h1 class="p-header__ttl"><a href="index.html">Hamburger</a></h1>
+        <form class="p-searchform">
+          <input type="search" class="p-searchform__keyword fas fa-search fa-lg" placeholder="&#xf002;">
+          <input type="submit" class="p-searchform__submit " value="検索">
+        </form>
+      </div>
+    </header>
+
+    <main class="l-main">
+
+      <!-- メインヴィジュアルエリア -->
+      <div class="p-hero--single c-mainvisual--page">
+        <h1 class="p-hero__ttl--single">ショップについて</h1>
+      </div>
+
+      <div class="l-container">
+      <!-- 導入エリア -->
+        <article class="p-intro">
+          <h2>見出しh2</h2>
+          <p>Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
+          <h3>見出しh3</h3>
+          <h4>見出しh4</h4>
+          <h5>見出しh5</h5>
+          <h6>見出しh6</h6>
+        </article>
+
+      <!-- 引用エリア -->
+        <blockquote class="p-blockquote">
+          <div class="p-blockquote__wrap">
+            <p class="p-blockquote__txt">
+                Blockquote
+                引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+            </p>
+            <p class="p-blockquote__link">
+                出典元：
+                <a href="#">○○○○○○○○○○○○</a>
+            </p>
+          </div>
+        </blockquote>
+
+        <!-- アイキャッチエリア -->
+        <article class="p-eyecatch">
+
+          <img src="/images/eyecatch/cooked-foods-750073@2x.jpg" alt="ハンバーガーの画像" class="p-eyecatch__top">
+
+          <ul class="p-eyecatch__list">
+            <li class="p-eyecatch__list__item">
+              <figure>
+                <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+                <figcaption>
+                  <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
+                </figcaption>
+              </figure>
+            </li>
+
+            <li class="p-eyecatch__list__item">
+              <figure class="c-card--eyecatch">
+                <figcaption class="c-card__caption--eyecatch">
+                  <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
+                </figcaption>
+                <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+              </figure>
+            </li>
+          </ul>
+
+          <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像" class="p-eyecatch__bottom">
+
+          <div class="p-eyecatch__gallery">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+            <img src="/images/eyecatch/cooked-foods-750073-2.png" alt="ハンバーガーの画像">
+          </div>
+
+        </article>
+
+        <!-- リストエリア -->
+        <div class="p-list">
+          <ol>
+              <li>1. リストリストリスト</li>
+              <li>2. リストリストリスト
+                  <ol>
+                      <li>1. リスト2リスト2リスト2</li>
+                      <li>2. リスト2リスト2リスト2</li>
+                  </ol>
+              </li>
+              <li>1. リストリストリスト</li>
+              <li>2. リストリストリスト</li>
+          </ol>
+
+          <ul>
+              <li>リストリストリスト</li>
+              <li>リストリストリスト
+                  <ul>
+                      <li>リスト2リスト2リスト2</li>
+                      <li>リスト2リスト2リスト2</li>
+                  </ul>
+              </li>
+              <li>リストリストリスト</li>
+              <li>リストリストリスト</li>
+          </ul>
+        </div>
+
+        <!-- コードエリア -->
+        <pre class="p-code">
+<code>
+          &lt;html&gt;
+                  &lt;head&gt;
+                  &lt;/head&gt;
+                  &lt;body&gt;
+                  &lt;/body&gt;
+          &lt;/html&gt;
+</code>
+        </pre>
+
+        <!-- テーブルエリア -->
+        <table class="p-table">
+          <tr>
+            <td>テーブル</td>
+            <td>テーブル</td>
+          </tr>
+          <tr>
+            <td>テーブル</td>
+            <td>テーブル</td>
+          </tr>
+          <tr>
+            <td>テーブル</td>
+            <td>テーブル</td>
+          </tr>
+          <tr>
+            <td>テーブル</td>
+            <td>テーブル</td>
+          </tr>
+        </table>
+        
+        <button class="c-btn">ボタン</button>
+
+        <!--  -->
+        <p class="p-bold">
+          boldboldboldboldboldboldbold
+        </p>
+
+      </div>
+    </main>
+
+    <!-- サイドバーエリア -->
+      <article class="l-sidebar">
+        <button class="c-btn--close c-btn--js"><span>閉じるボタン</span></button>
+        <h2 class="l-sidebar__ttl">Menu</h2>
+        <ul class="p-menulist">
+          <li class="p-menulist__item">
+            <h3>バーガー</h3>
+            <ul class="p-menulist__sub">
+              <li class="p-menulist__subitem"><a href="archive.html">ハンバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">チーズバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">テリヤキバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">アボガドバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">フィッシュバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">ベーコンバーガー</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">チキンバーガー</a></li>
+            </ul>
+          </li>
+
+          <li class="p-menulist__item">
+            <h3>サイド</h3>
+            <ul class="p-menulist__sub">
+              <li class="p-menulist__subitem"><a href="archive.html">ポテト</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">サラダ</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">ナゲット</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">コーン</a></li>
+            </ul>
+          </li>
+
+          <li class="p-menulist__item">
+            <h3>ドリンク</h3>
+            <ul class="p-menulist__sub">
+              <li class="p-menulist__subitem"><a href="archive.html">コーラ</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">ファンタ</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">オレンジ</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">アップル</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">紅茶（Ice/Hot)</a></li>
+              <li class="p-menulist__subitem"><a href="archive.html">コーヒー（Ice/Hot)</a></li>
+            </ul>
+          </li>
+        </ul>
+      </article>
+    </div>
+
+    <!-- フッターエリア -->
+    <footer class="l-footer">
+      <div class="p-footer">
+        <ul class="p-footer__menulist">
+          <li class="p-footer__menulist__item"><a href="page.html">ショップ情報</a></li>
+          <li class="p-footer__menulist__item"><a href="page.html">ヒストリー</a></li>
+        </ul>
+        <p><small>Copyright: RaiseTech</small></p>
+      </div>
+    </footer>
+
+  <script src="./js/menu.js"></script>
+  <script src="./js/responsive.js"></script>
+
+</body>
+</html>
