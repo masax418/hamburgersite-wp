99a119936011d733d989c49ee5e4fc12ccff78e7
masataka <masaburger0418@yahoo.co.jp>
2021-09-21 23:23:05 +0900

    カスタムメニュー表示

--- sidebar.php
+++ sidebar.php
@@ -2,41 +2,13 @@
 <article class="l-sidebar">
   <button class="c-btn--close c-btn--js"><span>閉じるボタン</span></button>
   <h2 class="l-sidebar__ttl">Menu</h2>
-  <ul class="p-menulist">
-    <li class="p-menulist__item">
-      <h3>バーガー</h3>
-      <ul class="p-menulist__sub">
-        <li class="p-menulist__subitem"><a href="archive.html">ハンバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">チーズバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">テリヤキバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">アボガドバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">フィッシュバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">ベーコンバーガー</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">チキンバーガー</a></li>
-      </ul>
-    </li>
-
-    <li class="p-menulist__item">
-      <h3>サイド</h3>
-      <ul class="p-menulist__sub">
-        <li class="p-menulist__subitem"><a href="archive.html">ポテト</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">サラダ</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">ナゲット</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">コーン</a></li>
-      </ul>
-    </li>
-
-    <li class="p-menulist__item">
-      <h3>ドリンク</h3>
-      <ul class="p-menulist__sub">
-        <li class="p-menulist__subitem"><a href="archive.html">コーラ</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">ファンタ</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">オレンジ</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">アップル</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">紅茶（Ice/Hot)</a></li>
-        <li class="p-menulist__subitem"><a href="archive.html">コーヒー（Ice/Hot)</a></li>
-      </ul>
-    </li>
-  </ul>
+  <?php
+  wp_nav_menu(array(
+    'theme_location' => 'global', //メニューの位置を指定
+    'menu_class' => 'p-menulist__sub', //任意のclass名を追加
+    'items_wrap' => '<ul class="%2$s">%3$s</ul>', //id属性の自動付与を解除（id削除しclassは残す）
+    'container' => false //不要なdivタグを削除
+  ));
+  ?>
 </article>
 </div>
