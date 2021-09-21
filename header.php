efcbba84f11fa237329cb275354e90cfd7c1a592
masataka <masaburger0418@yahoo.co.jp>
2021-09-21 23:24:53 +0900

    head部分調整

--- header.php
+++ header.php
@@ -2,15 +2,11 @@
 <html lang="<?php language_attributes();//サイトの言語を設定する ?>">
 
 <head>
-
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <!-- メディアクエリ -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- jQueryの読み込み -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
