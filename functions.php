801d6de03a5ab538729af1923ff09e4f99cca258
masataka <masaburger0418@yahoo.co.jp>
2021-09-28 09:50:57 +0900

    カスタムメニュー設定

--- functions.php
+++ functions.php
@@ -49,5 +49,5 @@ add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
 add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
 add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
 function my_css_attributes_filter($var) {
-	return is_array($var) ? array_intersect($var, array( 'p-menulist__subitem' )) : '';//p-menulist__subitemだけを残す
+	return is_array($var) ? array_intersect($var, array( '' )) : '';
 }
