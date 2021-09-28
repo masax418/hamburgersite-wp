9599054e7167c4dd08bebd45f574a9757af5124b
masataka <masaburger0418@yahoo.co.jp>
2021-09-28 09:51:48 +0900

    固定ページリンク先設定

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 <footer class="l-footer">
   <div class="p-footer">
     <ul class="p-footer__menulist">
-      <li class="p-footer__menulist__item"><a href="page.html">ショップ情報</a></li>
-      <li class="p-footer__menulist__item"><a href="page.html">ヒストリー</a></li>
+      <li class="p-footer__menulist__item"><a href="<?php echo get_page_link(64); ?>">ショップ情報</a></li>
+      <li class="p-footer__menulist__item"><a href="<?php echo get_page_link(66); ?>">ヒストリー</a></li>
     </ul>
     <p><small>Copyright: RaiseTech</small></p>
   </div>
